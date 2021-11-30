68bc7f14297719d71a3aa8a75324823c41fb0a7f
Sebastian Schmidt <isibboi@gmail.com>
2021-11-30 15:35:54 +0200

    Replace redundant trait bounds with `Model`.

--- wither/src/cursor.rs
+++ wither/src/cursor.rs
@@ -3,18 +3,17 @@ use std::task::{Context, Poll};
 
 use futures::stream::Stream;
 use mongodb::Cursor;
-use serde::de::DeserializeOwned;
 
 use crate::error::{Result, WitherError};
 use crate::Model;
 
 /// A cursor of model documents.
-pub struct ModelCursor<T: DeserializeOwned + Unpin + Send + Sync> {
+pub struct ModelCursor<T: Model> {
     cursor: Cursor<T>,
     marker: std::marker::PhantomData<T>,
 }
 
-impl<T: Model + DeserializeOwned + Unpin + Send + Sync> ModelCursor<T> {
+impl<T: Model> ModelCursor<T> {
     pub(crate) fn new(cursor: Cursor<T>) -> Self {
         Self { cursor, marker: std::marker::PhantomData }
     }
@@ -22,9 +21,9 @@ impl<T: Model + DeserializeOwned + Unpin + Send + Sync> ModelCursor<T> {
 
 // Impl Unpin on this container as we do not care about this container staying pinned,
 // only the underlying `Cursor` needs to remain pinned while we poll from this vantage point.
-impl<T: DeserializeOwned + Unpin + Send + Sync> Unpin for ModelCursor<T> {}
+impl<T: Model> Unpin for ModelCursor<T> {}
 
-impl<T: Model + DeserializeOwned + Unpin + Send + Sync> Stream for ModelCursor<T> {
+impl<T: Model> Stream for ModelCursor<T> {
     type Item = Result<T>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
